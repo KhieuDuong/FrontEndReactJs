b8c7947f40ebdb950abde8979f83e57050bd7d24
KhieuDuong <khieu.duongvan@gmail.com>
2022-12-05 16:33:14 +0900

    edit file http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://vercel.com/khieuduong/backend-nodejs/api",
+  baseURL: "https://backend-nodejs-pi.vercel.app/api",
   headers: {
     "Content-type": "application/json"
   }
